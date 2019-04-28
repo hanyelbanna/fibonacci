f5ec1fb8262e70c0aea6219d0a80bca261181243
hany elbana <h>
2019-04-28 07:31:49 +0300

    Cache results with Agent abstraction

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -1,4 +1,6 @@
 defmodule Fibonacci do
+  use Agent
+
   @moduledoc """
   Concurrent Fibonacci Server With Elixir.
   """
@@ -11,9 +13,20 @@ defmodule Fibonacci do
   @doc """
   - Accept given number more than or equal to 0
   - Seround fib function return with {:ok, } tuple
+  - Save new numbers in cached state at separate prossess with Agent abstraction
   """
   def calculate(n) when is_integer(n) and n >= 0 do
-    {:ok, fib(n, 0, 1)}
+    Agent.start_link(fn -> %{0 => 0, 1 => 1} end, name: __MODULE__)
+
+    if in_cached = Agent.get(__MODULE__, fn map -> Map.get(map, n) end) do
+      IO.puts("Cached: " <> inspect(Agent.get(__MODULE__, fn map -> map end)))
+      {:ok, in_cached}
+    else
+      result = fib(n, 0, 1)
+      IO.puts("Not Cached")
+      Agent.update(__MODULE__, fn map -> Map.put(map, n, result) end)
+      {:ok, result}
+    end
   end
 
   @doc """
@@ -45,6 +58,7 @@ defmodule Fibonacci do
   # def fib(n) when is_integer(n) and n > 1, do: fib(n - 1) + fib(n - 2)
   Fast way:
   - Using tail recursion
+  - Here we will go with fib function only one time with each number down to 1
   """
   def fib(n, first, second) when is_integer(n) and n > 1, do: fib(n - 1, second, first + second)
 end
